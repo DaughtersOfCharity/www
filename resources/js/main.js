22cea897f5f3f9d871d065174752113b728662fc
Boris SCHAPIRA <boris@schapira.dev>
2020-08-07 13:30:44 +0200

    Add carrousels

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,8 @@
 // Import local modules
 import "@modules/mobile-nav";
 import "@modules/lazyload";
+import "van11y-accessible-carrousel-aria";
+
+[...document.getElementsByClassName("js-carrousel")].forEach(x =>
+    van11yAccessibleCarrouselAria(x)
+);
